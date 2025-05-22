6196dcaacd924555a63febd23aa409112d03911c
cafel177 <zhanghaitaoty@163.com>
2025-05-22 09:31:59 +0800

    Fix

--- ExampleProject_MV/js/plugins/EventConditionPro.js
+++ ExampleProject_MV/js/plugins/EventConditionPro.js
@@ -54,11 +54,11 @@
  * MV：逻辑增强 类型 指令内容
  * 两用的指令，后缀为 _出现 的用于事件出现条件，后缀为 _事件 的用于事件页内
  * 
- * 1. 出现条件启用(出现)：让事件页使用自定义出现条件，要在事件页开头设置一个本指令并将参数设置为true
+ * 1. 启用(出现)：让事件页使用自定义出现条件，要在事件页开头设置一个本指令并将参数设置为true
  *    设置了本指令的事件页会完全使用自定义条件忽略RM原生条件，关闭则反之
  *    设置好后，当前事件页所有带有 (用于出现条件) 的指令都会被纳入考量，不用时关闭本指令即可，无需大量删除
  *    ♦ 启用：true/false
- *    ♦ 示例：逻辑增强 出现条件启用 true
+ *    ♦ 示例：逻辑增强 启用_出现 true
  * 
  * 2. 条件(两用)：通过条件列表设置一组[开关]，每一条的结果支持保存供后续取用
  *    ♦ 结果：写[开关]，比较结果会保存在这里，留空为不保存
@@ -68,6 +68,8 @@
  *    ♦ 比较对象1：开关、变量、物品、武器、防具、事件朝向/位置(玩家为-1，当前事件为0)、角色、按键用[序号]，临时变量、独立开关用[名字]，脚本直接写，金币和时间不用写
  *    ♦ 比较类型：开关类只分为等于和其他两种，物品、武器、防具、金币、时间、事件朝向比较值，角色、按键、脚本不写
  *    ♦ 比较对象2：开关类写[开关]，变量类和物品、武器、防具、金币、时间、事件位置写[变量]，按键写触发类型(Triggered、Repeated、其他都为Pressed)，事件朝向写方向(下、左、右、上)、角色、脚本不写
+ *    ♦ 示例：逻辑增强 条件_出现 内容
+ *    ♦ 示例：逻辑增强 条件_事件 内容
  * 
  * 3. 变量(两用)：通过变量列表设置一组[变量]，每一条的结果支持保存供后续取用
  *    ♦ 结果：写[变量]，运算结果会保存在这里
@@ -76,6 +78,8 @@
  *    ♦ 运算类型
  *    ♦ 运算对象2取负数
  *    ♦ 运算对象2：写[变量]，随机数是较大值，脚本不写
+ *    ♦ 示例：逻辑增强 变量_出现 内容
+ *    ♦ 示例：逻辑增强 变量_事件 内容
  * 
  * 4. 运算(两用)：对一组[开关]进行逻辑组合，每一条的结果支持保存供后续取用
  *    ♦ 结果：写[开关]，运算结果会保存在这里
@@ -84,6 +88,8 @@
  *    ♦ 操作类型
  *    ♦ 运算对象2取反
  *    ♦ 运算对象2：写[开关]
+ *    ♦ 示例：逻辑增强 运算_出现 内容
+ *    ♦ 示例：逻辑增强 运算_事件 内容
  * 
  * 5. 输出(两用)：将一个临时变量的值输出到控制台
  *    ♦ 名称
@@ -103,9 +109,9 @@
  *    ♦ 开启：true/false
  *    ♦ 示例：逻辑增强 独立开关 E true
  *   
- * 9. 触发按键(触发)：在并行处理的事件页内使用，可以自定义按键来触发事件
+ * 9. 按键(触发)：在并行处理的事件页内使用，可以自定义按键来触发事件
  *    ♦ 按键：填写KeyCode，可通过百度得到，如R为82
- *    ♦ 示例：逻辑增强 触发按键 82
+ *    ♦ 示例：逻辑增强 按键 82
  *  
  * @param condition
  * @text 条件转换
@@ -128,7 +134,7 @@
  * 
  * 
  * @command enable
- * @text 出现条件启用
+ * @text 启用_出现
  * @desc 让事件页使用自定义出现条件，要在事件页开头设置一个本指令并将参数设置为true
  * 设置了本指令的事件页会完全使用自定义条件忽略RM原生条件，关闭则反之
  * 设置好后，当前事件页所有带有 (用于出现条件) 的指令都会被纳入考量，不用时关闭本指令即可，无需大量删除
@@ -288,7 +294,7 @@
  * 
  * 
  * @command input
- * @text 触发按键(用于触发条件)
+ * @text 按键(用于触发条件)
  * @desc 在并行处理的事件页内使用，可以自定义按键来触发事件
  * 
  * @arg keyCode
@@ -387,7 +393,7 @@
  * 
  * @param checkTarget2
  * @text 比较对象2
- * @desc 开关类写[开关]，变量类和物品、武器、防具、金币、时间、事件位置写[变量]，按键写触发类型(Triggered、Repeated、其他都为Pressed)，事件朝向写方向(下、左、右、上)、角色、脚本不写
+ * @desc 开关类写[开关]，变量类和物品、武器、防具、金币、时间、事件位置写[变量]，按键写触发类型(被触发、被重复、其他都为被按下)，事件朝向写方向(下、左、右、上)、角色、脚本不写
  */
 
 /*~struct~Variable:
@@ -490,9 +496,24 @@ var EventConditionPro = EventConditionPro || {};
 EventConditionPro.pluginName = "EventConditionPro"
 EventConditionPro.param = PluginManager.parameters(EventConditionPro.pluginName);
 // 出现条件
-EventConditionPro.appearCondition = ["condition", "variable", "expression", "log", "submit"]
+EventConditionPro.enable = ["enable", "启用_出现"]
+// 出现条件
+EventConditionPro.submit = ["submit", "提交_出现"]
+// 出现条件
+EventConditionPro.appearCondition = [
+    "condition",
+    "条件_出现",
+    "variable",
+    "变量_出现",
+    "expression",
+    "运算_出现",
+    "log",
+    "输出_出现",
+    "submit",
+    "提交_出现"
+]
 // 触发条件
-EventConditionPro.triggers = ["input"]
+EventConditionPro.triggers = ["input", "按键"]
 
 // ============================================================================= //
 // 插件指令，用于事件页，this指向Game_Interpreter
@@ -506,11 +527,6 @@ if (Utils.RPGMAKER_NAME === 'MV') {
     Game_Interpreter.prototype.pluginCommand = function (command, args) {
         EventConditionPro_Game_Interpreter_pluginCommand.call(this, command, args);
         if (command === "逻辑增强") {
-            if (args[0] === "出现条件启用" && args.length === 2) {
-                EventConditionPro_ProcessPluginCommand(this, this, this.currentCommand())
-                return;
-            }
-
             if (args[0] === "条件_事件" && args.length === 2) {
                 EventConditionPro_ProcessPluginCommand(this, this, this.currentCommand())
                 return;
@@ -989,7 +1005,7 @@ var EventConditionPro_ProcessPluginCommand = function (event, outer, eventItem) 
     let commandName = null
     let args = null
     if (Utils.RPGMAKER_NAME === 'MV') {
-        let params = eventItem._params[0].split(" ");
+        let params = eventItem.parameters[0].split(" ");
         params.shift();
         commandName = params.shift();
         args = params
@@ -999,11 +1015,11 @@ var EventConditionPro_ProcessPluginCommand = function (event, outer, eventItem) 
         args = eventItem.parameters[3]
     }
 
-    // 出现条件启用
+    // 启用_出现
     if (commandName === "enable") {
         return args.enable === "true"
     }
-    else if (commandName === "出现条件启用") {
+    else if (commandName === "启用_出现") {
         return args[0] === "true"
     }
     // 条件
@@ -1269,10 +1285,10 @@ var EventConditionPro_ProcessPluginCommand = function (event, outer, eventItem) 
                 }
                 // 触发类型
                 let re = false;
-                if (checkType === "Triggered") {
+                if (checkType === "被触发") {
                     re = Input.isTriggered(keyName)
                 }
-                else if (checkType === "Repeated") {
+                else if (checkType === "被重复") {
                     re = Input.isRepeated(keyName)
                 }
                 else {
@@ -1373,11 +1389,11 @@ var EventConditionPro_ProcessPluginCommand = function (event, outer, eventItem) 
         }
         return EventConditionPro_GetTempValue(outer, name)
     }
-    // 触发按键
-    else if (commandName === "input" || commandName === "触发按键") {
+    // 按键
+    else if (commandName === "input" || commandName === "按键") {
         let type = null
         let keyCode = null
-        if (commandName === "触发按键") {
+        if (commandName === "按键") {
             type = "Pressed"
             keyCode = String(args[0]);
         }
@@ -1432,7 +1448,7 @@ var EventConditionPro_ProcessConditions = function (event, outer, conditions) {
         let re = EventConditionPro_ProcessPluginCommand(event, outer, conditions[index])
         const commandName = conditions[index].parameters[1]
         // submit提交
-        if (commandName === "submit") {
+        if (EventConditionPro.submit.includes(commandName)) {
             EventConditionPro_ClearTempValue(outer)
             return re
         }
@@ -1475,7 +1491,7 @@ var EventConditionPro_Load = function (event, outer) {
             if (pluginName.includes(EventConditionPro.pluginName)) {
                 const pluginCommand = outer.list[index].parameters[1]
                 // 是enbale
-                if (pluginCommand === "enable") {
+                if (EventConditionPro.enable.includes(pluginCommand)) {
                     // 要考虑input，所以false下不能直接结束
                     if (EventConditionPro_ProcessPluginCommand(event, outer, outer.list[index])) {
                         Enable = true
